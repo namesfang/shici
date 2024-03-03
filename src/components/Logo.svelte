a2c62f37ac8d08d992c681da94aa148aba7a6c0b
namo <jixiang_f@163.com>
2024-03-03 19:52:36 +0800

    logo组件

---
+++ src/components/Logo.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let size = 194;
+  export let large = false;
+  export let medium = false;
+  export let small = false;
+
+  let src = '';
+  $: {
+    const sizes = [large, medium, small]
+    const nums = [194, 114, 72]
+
+    for(let i in sizes) {
+      if(sizes[i]) {
+        size = nums[i]
+        break;
+      }
+    }
+
+    src = `/images/logo-${size}.jpg`
+  }
+</script>
+
+<img {src} alt="LOGO"/>
