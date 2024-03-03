b481eb70c59314509c016596b1b101c5c74391ae
namo <jixiang_f@163.com>
2024-03-03 19:52:41 +0800

    分页组件

--- src/components/Pagination.svelte
+++ src/components/Pagination.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { page } from "$app/stores";
   type Pages = {
     text: string;
     num: number;
@@ -17,8 +18,9 @@
 
   $: pageCount = count > 0 ? Math.ceil(count / take) : 0;
 
+  $: pageCurrent = $page.url.searchParams.get('page') ?? 1;
+
   $: {
-    
     if(pageCount <= pagLength) {
       for(let i=1; i<=pagLength; i++) {
         pages.push({
@@ -48,17 +50,26 @@
     }
   }
 
-</script>
+  
 
-<div class="">
+</script>
+<div class="pagination">
   <p>共{pageCount}页，每页{take}条</p>
-  <input type="number" />
+  <form>
+    <input type="number" name="page" placeholder="跳转"/>
+  </form>
   <ul>
     {#each pages as item}
       {#if item.num > 0}
-        <li>
-          <a href="{url}?page={item.num}">{item.text}</a>
-        </li>
+        {#if pageCurrent == item.num}
+          <li class="active">
+            <span>{item.text}</span>
+          </li>
+        {:else}
+          <li>
+            <a href="{url}?page={item.num}">{item.text}</a>
+          </li>
+        {/if}
       {:else}
         <li>
           <span>{item.text}</span>
@@ -67,3 +78,51 @@
     {/each}
   </ul>
 </div>
+
+<style lang="scss">
+  .pagination {
+    height: 46px;
+    display: flex;
+    justify-content: right;
+    align-items: center;
+    p {
+      font-size: 14px;
+      color: var(--gray-700);
+    }
+    form {
+      padding: 0 20px;
+      input {
+        width: 50px;
+        height: 30px;
+        padding: 0 10px;
+        font-size: 14px;
+        border: 1px solid var(--gray-400);
+        border-radius: 5px;
+      }
+    }
+
+    ul {
+      display: flex;
+      li {
+        a, span {
+          font-size: 14px;
+          color: var(--gray-700);
+          display: flex;
+          padding: 8px 12px;
+          margin-right: 6px;
+        }
+
+        span {
+          color: var(--gray-400);
+        }
+      }
+
+      .active {
+        span {
+          color: var(--white);
+          background-color: var(--primary-500);
+        }
+      }
+    }
+  }
+</style>
