a80b2c894ecc6bcf4fa7151ff46b541d607aadb7
Namo <jixiang.f@gmail.com>
2024-03-14 17:27:32 +0800

    首页优化

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
+	import SearchBox from "$component/SearchBox.svelte";
+
   export let data;
 </script>
 
 <svelte:head>
-  <title>{data.locals.title}|首页</title>
+  <title>{data.locals.title}</title>
 </svelte:head>
 
 <div class="wrapper">
+  <SearchBox />
 </div>
 
 <style lang="scss" scoped>
   .wrapper {
     height: calc(100vh - var(--height-header) - var(--height-footer));
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 </style>
