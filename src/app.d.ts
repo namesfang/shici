ea4120a8cbceebe06d894db85000630ae87af747
Namo <jixiang.f@gmail.com>
2024-03-15 15:49:38 +0800

    添加类型声明

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,21 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
+
+	type CommonItem = {
+		value: string;
+		label: string;
+	}
+
+	type DictMap = {
+		[key: string]: CommonItem[]
+	}
+	
 	namespace App {
 		// interface Error {}
 		interface Locals {
 			title: string
+			dict: DictMap
 			user?: {
 				id: number,
 				fullname: string,
