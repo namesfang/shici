1c6b421b8e441adceb0c6c3d2fe0d07009969c19
Namo <jixiang.f@gmail.com>
2024-03-18 13:41:01 +0800

    fix: 验证码

--- src/routes/api/correction/+server.ts
+++ src/routes/api/correction/+server.ts
@@ -1,13 +1,32 @@
-import { client } from '$lib/prisma.js'
+import { client } from '$lib/prisma'
+import { client as redisClient } from '$lib/redis'
 import { json } from '@sveltejs/kit'
 
-export async function POST({ request }) {
-  const { postId, type, content } = await request.json()
+export async function POST({ request, cookies }) {
+  const { postId, type, captcha, content } = await request.json()
 
-  if(!type || !content) {
+  if(!type || !content || !captcha) {
     return json({
       msg: '数据不完整',
       code: 1
+    })
+  }
+
+  const sessionid = cookies.get('sessionid')
+
+  const text = await (await redisClient()).get(`captcha:${sessionid}`)
+
+  if(!text) {
+    return json({
+      msg: '验证码异常',
+      code: 1,
+    })
+  }
+  
+  if(text.toLowerCase() !== captcha.toLowerCase()) {
+    return json({
+      msg: '验证码不正确',
+      code: 1,
     })
   }
 
