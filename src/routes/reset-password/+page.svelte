fc0e2ee5532eda72a06ee6f9ca73f5583486044b
Namo <jixiang.f@gmail.com>
2024-03-19 17:11:16 +0800

    忘记密码页面

---
+++ src/routes/reset-password/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import AdminQr from "$component/AdminQR.svelte";
+</script>
+
+<div class="wrapper">
+  <AdminQr/>
+</div>
+
+<style lang="scss">
+  .wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: calc(100vh - var(--height-header) - var(--height-footer));
+  }
+</style>
