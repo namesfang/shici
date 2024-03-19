c0cc4794629a7ec0fbf1aa543494d0fc48f01c83
Namo <jixiang.f@gmail.com>
2024-03-19 11:37:26 +0800

    cls:

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,9 +2,10 @@ import { client } from "$lib/prisma"
 
 export const load = async({ locals })=> {
 
+  // 诗人总量
   const authorCount = await client.author.count()
+  // 诗词总量
   const postCount = await client.post.count()
-
   // 获取导航
   const links = await client.dynasty.findMany()
 
