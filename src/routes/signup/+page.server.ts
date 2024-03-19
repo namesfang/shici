2abb7cba685803f44999297f95329559f0674fb8
Namo <jixiang.f@gmail.com>
2024-03-19 17:11:54 +0800

    新增注册页面

--- src/routes/signup/+page.server.ts
+++ src/routes/signup/+page.server.ts
@@ -1,29 +1,77 @@
-import { fail } from "@sveltejs/kit"
-import type { Actions } from "./$types"
+import { fail, redirect } from "@sveltejs/kit"
+import { client as redisClient } from '$lib/redis'
+import { client } from "$lib/prisma"
+import { hashSync } from 'bcrypt'
 
 export const actions = {
-  async default({ request }) {
-    const data = await request.formData()
+  async default({ request, locals }) {
+    try {
+      const data = await request.formData()
 
-    const keys: {[name: string]: string} = {
-      username: '用户名',
-      nickname: '昵称',
-      password: '密码'
-    }
+      const keys: {[name: string]: string} = {
+        fullname: '用户名',
+        password: '密码',
+        captcha: '验证码',
+      }
 
-    const errors = [];
-    for(const key in keys) {
-      const length = (data.get(key) as string).length
-      if(!data.has(key) || length === 0) {
-        errors.push(`${keys[key]}不能为空`)
-        continue
+      const errors = [];
+      for(const key in keys) {
+        const length = (data.get(key) as string).length
+        if(!data.has(key) || length === 0) {
+          errors.push(`${keys[key]}不能为空`)
+          continue
+        }
+        if(length < 1) {
+          errors.push(`${keys[key]}至少2位`)
+        }
       }
-      if(length < 1) {
-        errors.push(`${keys[key]}至少2位`)
+
+      if(errors.length > 0) {
+        return fail(422, {
+          errors
+        })
       }
+
+      const captcha = data.get('captcha') as string;
+
+      const text = await (await redisClient()).get(`captcha:${locals.sessionid}`)
+
+      if(!text) {
+        return fail(422, {
+          errors: ['验证码异常']
+        })
+      }
+
+      if(text.toLowerCase() !== captcha.toLowerCase()) {
+        return fail(422, {
+          errors: ['验证码不正确']
+        })
+      }
+
+      const fullname = data.get('fullname') as string;
+
+      // 是否有重名的
+      const where = {
+        fullname,
+        frozen: false
+      }
+      if(await client.user.count({ where })) {
+        return fail(422, {
+          errors: ['账号已存在']
+        })
+      }
+
+      await client.user.create({
+        data: {
+          fullname,
+          hash: hashSync(data.get('password') as string, 5),
+        }
+      })
+      return redirect(302, '/login')
+    } catch (error) {
+      return fail(422, {
+        errors: ['系统异常']
+      })
     }
-    return fail(400, {
-      errors
-    })
   }
-} satisfies Actions
+}
