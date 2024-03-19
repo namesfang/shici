43b73d7ae01e3748196aa96bc310f3dc60d6bd4d
Namo <jixiang.f@gmail.com>
2024-03-19 18:15:14 +0800

    添加登录页面

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,9 +1,90 @@
-export async function load() {
-  return {}
-}
+import { fail, redirect } from "@sveltejs/kit"
+import { client as redisClient } from '$lib/redis'
+import { client } from "$lib/prisma"
+import { compareSync } from 'bcrypt'
 
 export const actions = {
-  default(e) {
-    console.log(e)
+  async default({ request, locals }) {
+    try {
+      const data = await request.formData()
+
+      const keys: {[name: string]: string} = {
+        fullname: '用户名',
+        password: '密码',
+        captcha: '验证码',
+      }
+
+      const errors = [];
+      for(const key in keys) {
+        const length = (data.get(key) as string).length
+        if(!data.has(key) || length === 0) {
+          errors.push(`${keys[key]}不能为空`)
+          continue
+        }
+        if(length < 1) {
+          errors.push(`${keys[key]}至少2位`)
+        }
+      }
+
+      if(errors.length > 0) {
+        return fail(422, {
+          errors
+        })
+      }
+
+      const captcha = data.get('captcha') as string;
+
+      const redis = await redisClient()
+
+      const text = await redis.get(`captcha:${locals.sessionid}`)
+
+      if(!text) {
+        return fail(422, {
+          errors: ['验证码异常']
+        })
+      }
+
+      if(text.toLowerCase() !== captcha.toLowerCase()) {
+        return fail(422, {
+          errors: ['验证码不正确']
+        })
+      }
+
+      const info = await client.user.findFirst({
+        where: {
+          fullname: data.get('fullname') as string,
+          frozen: false
+        }
+      });
+
+      if(null === info) {
+        return fail(422, {
+          errors: ['系统未找到此账号']
+        })
+      }
+      const password = data.get('password') as string;
+
+      if(!compareSync(password, info.hash)) {
+        return fail(422, {
+          errors: ['系统未找到此账号']
+        })
+      }
+
+      await redis.hSet(`user:${locals.sessionid}`, {
+        id: info.id,
+        fullname: info.fullname,
+        adm: info.adm ? 1 : 0,
+        createAt: info.createdAt.toString()
+      })
+
+      await redis.disconnect();
+
+      return redirect(302, '/user')
+      
+    } catch (error) {
+      return fail(422, {
+        errors: ['系统异常']
+      })
+    }
   }
 }
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,19 +1,53 @@
 <script lang="ts">
-	import Icon from "$component/Icon.svelte";
 	import Logo from "$component/Logo.svelte";
 
+	import { onMount } from "svelte";
 
+  export let data;
+  export let form;
+
+  $: locals = data.locals
+
+  // 获取验证码接口
+  let svgHtml = '';
+  const fetchCaptcha = async ()=> {
+    svgHtml = await (await fetch('/api/captcha')).text()
+  }
+
+  onMount(()=> {
+    fetchCaptcha()
+  })
 </script>
 
+<svelte:head>
+  <title>账号登录|{locals.title}</title>
+</svelte:head>
+
 <div class="login-wrapper">
-    <form>
+    <form method="post">
       <div class="hd">
         <Logo small/>
       </div>
       <div class="md">
-        <input type="text" placeholder="账号" autocomplete="off"/>
-        <input type="password" placeholder="密码"/>
+        <input name="fullname" type="text" placeholder="账号" autocomplete="off"/>
+        <input name="password" type="password" placeholder="密码"/>
       </div>
+      <div class="rp">
+        <a href="/reset-password">忘记密码?</a>
+      </div>
+      <div class="pt">
+        <input name="captcha" type="text" maxlength="4" placeholder="验证码"/>
+        <button on:click={ fetchCaptcha } type="button" title="点击刷新验证码">
+          {@html svgHtml}
+        </button>
+      </div>
+      {#if Array.isArray(form?.errors) }
+        <ul class="errors">
+          {#each form.errors as error}
+          <li>{error}</li>
+          {/each}
+        </ul>
+      {/if}
       <div class="ft">
         <button type="submit">登录</button>
       </div>
@@ -25,48 +59,89 @@
 </div>
 
 <style lang="scss">
-
   .login-wrapper {
     height: calc(100vh - var(--height-header) - var(--height-footer));
     display: flex;
     align-items: center;
     justify-content: center;
 
+    .errors {
+      margin-bottom: 40px;
+      li {
+        line-height: 30px;
+        text-align: left;
+        color: var(--primary-900);
+      }
+    }
+
     form {
       width: 320px;
       display: flex;
       flex-direction: column;
       align-items: center;
+      padding: 32px 24px;
+      background-color: var(--gray-50);
+      border: 1px solid var(--gray-100);
+      border-radius: 10px;
+
+      input {
+        width: 100%;
+        height: 46px;
+        padding: 0 20px 0 40px;
+        box-sizing: border-box;
+        position: relative;
+        z-index: 0;
+        
+        &:first-child {
+          border-radius: 10px 10px 0 0;
+          border: 1px solid var(--gray-300);
+        }
+        &:last-child {
+          border-radius: 0 0 10px 10px;
+          border: 1px solid var(--gray-300);
+          border-top: 0;
+        }
+        &:focus {
+          border-color: transparent;
+          outline: 3px solid var(--primary-900);
+          z-index: 1;
+        }
+      }
+      
       .hd {
         height: 72px;
       }
+
       .md {
-        margin: 40px 0;
-        
-        input {
-          width: 100%;
-          height: 46px;
-          padding: 0 20px 0 40px;
-          box-sizing: border-box;
-          position: relative;
-          z-index: 0;
-          
-          &:first-child {
-            border-radius: 10px 10px 0 0;
-            border: 1px solid var(--gray-400);
-          }
-          &:last-child {
-            border-radius: 0 0 10px 10px;
-            border: 1px solid var(--gray-400);
-            border-top: 0;
-          }
-          &:focus {
-            border-color: transparent;
-            outline: 3px solid var(--primary-900);
-            z-index: 1;
-          }
+        margin: 40px 0 10px;
+      }
+
+      .rp {
+        width: 100%;
+        text-align: right;
+        margin: 0 0 20px;
+        a {
+          font-size: 14px;
+          color: var(--primary-900);
         }
       }
+
+      .pt {
+        margin-bottom: 40px;
+        display: flex;
+        input {
+          width: 150px;
+          border-radius: 10px;
+        }
+        button {
+          width: 150px;
+          border-radius: 10px;
+          border: 0;
+          background-color: transparent;
+          margin-left: 20px;
+        }
+      }
+
       .ft {
         width: 100%;
         height: 46px;
