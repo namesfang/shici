db20deb6e834ca7178c2febacd516c76b3938a0b
Namo <jixiang.f@gmail.com>
2024-03-20 14:20:19 +0800

    个人中心删除

---
+++ src/routes/api/favorite/delete/+server.ts
@@ -0,0 +1,31 @@
+import { client } from '$lib/prisma.js'
+import { json } from '@sveltejs/kit'
+
+export async function POST({ request, locals }) {
+  const { id } = await request.json()
+
+  if(!id) {
+    return json({
+      code: 1,
+      msg: '表单数据不完整'
+    })
+  }
+
+  try {
+      await client.userFavorite.delete({
+        where: {
+          id,
+          userId: locals.user!.id
+        }
+      })
+      return json({
+        code: 0,
+        msg: '已取消收藏',
+      })
+  } catch (error) {
+    return json({
+      code: 1,
+      msg: '系统异常'
+    })
+  }
+}
