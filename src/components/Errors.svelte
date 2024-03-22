71d1898233a14fade59cf1bfcd77aee9e7df2951
Namo <jixiang.f@gmail.com>
2024-03-22 14:31:34 +0800

    新增Errors组件

---
+++ src/components/Errors.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let errors: string[];
+</script>
+
+<ul class="errors">
+  {#each errors as error}
+  <li>{error}</li>
+  {/each}
+</ul>
+
+<style lang="scss">
+  .errors {
+    margin-bottom: 40px;
+    li {
+      line-height: 30px;
+      text-align: left;
+      color: var(--primary-900);
+    }
+  }
+</style>
