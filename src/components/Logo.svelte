899a199a787bb888065571ff25bad6cd8a2169c4
Namo <jixiang.f@gmail.com>
2024-03-22 14:31:45 +0800

    Logo组件添加新风格

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -2,9 +2,14 @@
   import { page } from '$app/stores'
 
   export let size = 194;
+  
   export let large = false;
   export let medium = false;
   export let small = false;
+
+  export let circle = false;
+  export let round = false;
+
   export let className = ''
 
   let src = '';
@@ -23,4 +28,13 @@
   }
 </script>
 
-<img {src} alt={$page.data.locals.title} title={$page.data.locals.title} class={className}/>
+<img {src} alt={$page.data.locals.title} title={$page.data.locals.title} class:round={ round } class:circle={ circle } class={className}/>
+
+<style>
+  .round {
+    border-radius: 16px;
+  }
+  .circle {
+    border-radius: 50%;
+  }
+</style>
