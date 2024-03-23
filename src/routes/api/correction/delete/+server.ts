96acd7c2560e745c28ec6faaad9daf6340142612
namo <jixiang_f@163.com>
2024-03-23 17:49:51 +0800

    纠错删除

---
+++ src/routes/api/correction/delete/+server.ts
@@ -0,0 +1,31 @@
+import { client } from '$lib/prisma'
+import { json } from '@sveltejs/kit'
+
+export async function POST({ request, locals }) {
+  const { id } = await request.json()
+
+  if(!id) {
+    return json({
+      code: 1,
+      msg: '表单数据不完整'
+    })
+  }
+
+  try {
+    await client.correction.delete({
+      where: {
+        id,
+        userId: locals.user!.id
+      }
+    })
+    return json({
+      code: 0,
+      msg: '已取消',
+    })
+  } catch (error) {
+    return json({
+      code: 1,
+      msg: '系统异常'
+    })
+  }
+}
